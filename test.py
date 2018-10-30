b2c37a06effafbb0940e2f258036c7355c7c2ee8
nift4 <me@nift4.org>
2018-10-30 19:15:39 +0100

    Init commit2

---
+++ test.py
@@ -0,0 +1,15 @@
+from pitftgpio import PiTFT_GPIO as PiTFT
+import os
+pitft=PiTFT()
+try:
+    while True:
+        if pitft.Button1:
+            os.system("sudo fbi -T 2 -a -noverbose -d /dev/fb1 ~pi/adapiluv320x240.jpg")
+        if pitft.Button2:
+            os.system("sudo fbi -T 2 -a -noverbose -d /dev/fb1 ~pi/s2c.gif")
+        if pitft.Button3:
+            pass
+        if pitft.Button4:
+            pass
+except KeyboardInterrupt:
+    print("bye")
